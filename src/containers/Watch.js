7fe1b7b3cd880e2f65af025b838c8718826761f2
simonsankar <simonlfcsuarez7@gmail.com>
2018-02-06 23:29:06 -0400

    Watch page updated

--- src/containers/Watch.js
+++ src/containers/Watch.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Grid, Segment, Dimmer, Loader } from 'semantic-ui-react';
+import {
+  Grid,
+  Segment,
+  Dimmer,
+  Loader,
+  Header,
+  Image
+} from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -11,7 +18,7 @@ import { getEpisodes, resestEpisodes } from '../actions/getEpisodes';
 import { resetVideo } from '../actions/getVideo';
 
 import EpisodesMenu from './EpisodesMenu';
-import { medHeight } from '../styles/column.css';
+import { vidHeight } from '../styles/column.css';
 
 class Watch extends Component {
   componentDidMount() {
@@ -42,40 +49,67 @@ class Watch extends Component {
     const { currentVideo } = this.props;
     return (
       <Grid container>
-        <Grid.Column computer={11} tablet={11} mobile={16}>
-          <Segment.Group>
-            <Segment color="teal">
-              {anime ? <h4>{anime.title}</h4> : <h4>...</h4>}
-            </Segment>
-            <Segment clearing style={medHeight}>
-              {!currentVideo ? (
+        <Grid.Row columns={2}>
+          <Grid.Column computer={11} tablet={11} mobile={16}>
+            <Segment.Group>
+              <Segment color="teal">
+                {anime ? <h4>{anime.title}</h4> : <h4>...</h4>}
+              </Segment>
+              <Segment clearing style={vidHeight}>
+                {!currentVideo ? (
+                  <Dimmer inverted active={true}>
+                    {anime ? (
+                      <Image
+                        src={anime.cover}
+                        centered
+                        size="large"
+                        rounded
+                        bordered
+                      />
+                    ) : null}
+                    <Loader inverted />
+                  </Dimmer>
+                ) : (
+                  <iframe
+                    title={anime ? anime.title : 'nothing'}
+                    src={currentVideo}
+                    frameBorder="0"
+                    allowFullScreen="true"
+                    style={{ height: '60vh', width: '100%' }}
+                  />
+                )}
+              </Segment>
+            </Segment.Group>
+            <Segment clearing style={vidHeight} className="fade-in">
+              {!anime ? (
                 <Dimmer inverted active={true}>
                   <Loader inverted />
                 </Dimmer>
               ) : (
-                <iframe
-                  title={anime ? anime.title : 'nothing'}
-                  src={currentVideo}
-                  frameBorder="0"
-                  allowFullScreen="true"
-                  style={{ height: '60vh', width: '100%' }}
-                />
+                <Grid>
+                  <Grid.Row columns={2} divided verticalAlign="middle">
+                    <Grid.Column computer={11} tablet={10} mobile={16}>
+                      <Header as="h5">
+                        <Header.Content>
+                          {anime.title}
+                          <Header.Subheader>{anime.alias}</Header.Subheader>
+                        </Header.Content>
+                      </Header>
+
+                      <p style={{ fontSize: '.85rem' }}>{anime.desc}</p>
+                    </Grid.Column>
+                    <Grid.Column computer={5} tablet={6} mobile={16}>
+                      <Image bordered centered fluid rounded src={anime.img} />
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
               )}
             </Segment>
-          </Segment.Group>
-          <Segment clearing style={medHeight} className="fade-in">
-            {!anime ? (
-              <Dimmer inverted active={true}>
-                <Loader inverted />
-              </Dimmer>
-            ) : (
-              <p style={{ fontSize: '.85rem' }}>{anime.desc}</p>
-            )}
-          </Segment>
-        </Grid.Column>
-        <Grid.Column computer={5} tablet={5} mobile={16}>
-          <EpisodesMenu />
-        </Grid.Column>
+          </Grid.Column>
+          <Grid.Column computer={5} tablet={5} mobile={16}>
+            <EpisodesMenu />
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
